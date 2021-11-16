da4b953a5acd8c2c2536edc5ebc6df819cf95b2d
naal01002 <89377694+naal01002@users.noreply.github.com>
2021-11-16 13:36:20 +0100

    Abs(Abs) = Abs

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace jag_är_inte_du
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //yoooooo
+            //dhago tfdoögd
+
+            int a = 333;
+            int b = 332;
+
+            Suwitchu(ref a, ref b);
+
+            Console.WriteLine(a + " " + b);
+        }
+
+        static void Suwitchu(ref int a, ref int b){
+            int a2 = a;
+            a = b;
+            b = a2;
+        }
+    }
+}
